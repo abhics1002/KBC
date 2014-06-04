65dcda8e9701ff55e165bf770d5266a39d05304e
unknown <abbharadwaj@L-MAA-00438579.corp.ebay.com>
2014-06-04 20:03:07 +0530

    back ground sound added

--- gen/com/example/kbc/R.java
+++ gen/com/example/kbc/R.java
@@ -19,13 +19,13 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int metaButtonBarStyle=0x7f010000;
     }
     public static final class color {
-        public static final int black_overlay=0x7f040000;
+        public static final int black_overlay=0x7f050000;
     }
     public static final class dimen {
-        public static final int button_padding=0x7f050003;
-        public static final int question_button_padding=0x7f050002;
-        public static final int score_padding=0x7f050000;
-        public static final int table_padding=0x7f050001;
+        public static final int button_padding=0x7f060003;
+        public static final int question_button_padding=0x7f060002;
+        public static final int score_padding=0x7f060000;
+        public static final int table_padding=0x7f060001;
     }
     public static final class drawable {
         public static final int button_border=0x7f020000;
@@ -33,59 +33,62 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int oval_button=0x7f020002;
     }
     public static final class id {
-        public static final int button1=0x7f08001a;
-        public static final int button2=0x7f08001b;
-        public static final int button3=0x7f08001c;
-        public static final int button4=0x7f08001d;
-        public static final int button5=0x7f080012;
-        public static final int button6=0x7f080014;
-        public static final int button7=0x7f080017;
-        public static final int button8=0x7f080018;
-        public static final int button9=0x7f080015;
-        public static final int questiontable=0x7f080010;
-        public static final int tableLayout1=0x7f080019;
-        public static final int tableRow1=0x7f080011;
-        public static final int tableRow2=0x7f080013;
-        public static final int tableRow3=0x7f080016;
-        public static final int textView1=0x7f080001;
-        public static final int textView10=0x7f08000a;
-        public static final int textView11=0x7f08000b;
-        public static final int textView12=0x7f08000c;
-        public static final int textView13=0x7f08000d;
-        public static final int textView14=0x7f08000e;
-        public static final int textView15=0x7f08000f;
-        public static final int textView2=0x7f080002;
-        public static final int textView3=0x7f080003;
-        public static final int textView4=0x7f080004;
-        public static final int textView5=0x7f080005;
-        public static final int textView6=0x7f080006;
-        public static final int textView7=0x7f080007;
-        public static final int textView8=0x7f080008;
-        public static final int textView9=0x7f080009;
-        public static final int verticalbar=0x7f080000;
+        public static final int button1=0x7f09001a;
+        public static final int button2=0x7f09001b;
+        public static final int button3=0x7f09001c;
+        public static final int button4=0x7f09001d;
+        public static final int button5=0x7f090012;
+        public static final int button6=0x7f090014;
+        public static final int button7=0x7f090017;
+        public static final int button8=0x7f090018;
+        public static final int button9=0x7f090015;
+        public static final int questiontable=0x7f090010;
+        public static final int tableLayout1=0x7f090019;
+        public static final int tableRow1=0x7f090011;
+        public static final int tableRow2=0x7f090013;
+        public static final int tableRow3=0x7f090016;
+        public static final int textView1=0x7f090001;
+        public static final int textView10=0x7f09000a;
+        public static final int textView11=0x7f09000b;
+        public static final int textView12=0x7f09000c;
+        public static final int textView13=0x7f09000d;
+        public static final int textView14=0x7f09000e;
+        public static final int textView15=0x7f09000f;
+        public static final int textView2=0x7f090002;
+        public static final int textView3=0x7f090003;
+        public static final int textView4=0x7f090004;
+        public static final int textView5=0x7f090005;
+        public static final int textView6=0x7f090006;
+        public static final int textView7=0x7f090007;
+        public static final int textView8=0x7f090008;
+        public static final int textView9=0x7f090009;
+        public static final int verticalbar=0x7f090000;
     }
     public static final class layout {
         public static final int activity_game=0x7f030000;
     }
+    public static final class raw {
+        public static final int test=0x7f040000;
+    }
     public static final class string {
-        public static final int app_name=0x7f060000;
-        public static final int dummy_button=0x7f060001;
-        public static final int dummy_content=0x7f060002;
-        public static final int eighty_K=0x7f06000d;
-        public static final int five_K=0x7f060011;
-        public static final int fivecrore=0x7f060004;
-        public static final int fivetylac=0x7f060007;
-        public static final int fourty_K=0x7f06000e;
-        public static final int onecrore=0x7f060006;
-        public static final int onelac=0x7f06000c;
-        public static final int sevencrore=0x7f060003;
-        public static final int sixlac=0x7f06000a;
-        public static final int ten_K=0x7f060010;
-        public static final int threecrore=0x7f060005;
-        public static final int threelac=0x7f06000b;
-        public static final int twelvelac=0x7f060009;
-        public static final int twenty_K=0x7f06000f;
-        public static final int twentyfivelac=0x7f060008;
+        public static final int app_name=0x7f070000;
+        public static final int dummy_button=0x7f070001;
+        public static final int dummy_content=0x7f070002;
+        public static final int eighty_K=0x7f07000d;
+        public static final int five_K=0x7f070011;
+        public static final int fivecrore=0x7f070004;
+        public static final int fivetylac=0x7f070007;
+        public static final int fourty_K=0x7f07000e;
+        public static final int onecrore=0x7f070006;
+        public static final int onelac=0x7f07000c;
+        public static final int sevencrore=0x7f070003;
+        public static final int sixlac=0x7f07000a;
+        public static final int ten_K=0x7f070010;
+        public static final int threecrore=0x7f070005;
+        public static final int threelac=0x7f07000b;
+        public static final int twelvelac=0x7f070009;
+        public static final int twenty_K=0x7f07000f;
+        public static final int twentyfivelac=0x7f070008;
     }
     public static final class style {
         /** 
@@ -99,10 +102,10 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     </style>
  
          */
-        public static final int AppTheme=0x7f070000;
-        public static final int ButtonBar=0x7f070002;
-        public static final int ButtonBarButton=0x7f070003;
-        public static final int FullscreenTheme=0x7f070001;
+        public static final int AppTheme=0x7f080000;
+        public static final int ButtonBar=0x7f080002;
+        public static final int ButtonBarButton=0x7f080003;
+        public static final int FullscreenTheme=0x7f080001;
     }
     public static final class styleable {
         /** 
--- src/com/example/kbc/GameActivity.java
+++ src/com/example/kbc/GameActivity.java
@@ -3,7 +3,11 @@ package com.example.kbc;
 import com.example.kbc.util.SystemUiHider;
 
 
+
+
 import android.app.Activity;
+import android.media.MediaPlayer;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 /**
@@ -14,15 +18,58 @@ import android.os.Bundle;
  */
 public class GameActivity extends Activity {
 
-
+	BackgroundSound mBackgroundSound = null;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.activity_game);
-
+		mBackgroundSound = new BackgroundSound();
+		mBackgroundSound.execute(null,null,null);
 		
 	}
+	
+	
+	public class BackgroundSound extends AsyncTask<Void, Void, Void> {
+		MediaPlayer player = MediaPlayer.create(GameActivity.this, R.raw.test); 
+		@Override
+		protected Void doInBackground(Void... params) {
+		   // MediaPlayer player = MediaPlayer.create(GameActivity.this, R.raw.test); 
+		    player.setLooping(true); // Set looping 
+		    player.setVolume(100,100); 
+		    player.start(); 
+		    return null;
+		 }
+		
+		
+		public void stopMusic()
+		{
+			player.stop();
+			player.release();
+		}
+		   }
+	
+	/*public void onResume() {
+		super.onResume();
+		mBackgroundSound.execute(null,null,null);
+		}*/
+	
+	 public void onPause() {
+		 super.onPause();
+		 mBackgroundSound.cancel(true);
+		 }
+	 
+	 public void onDestroy()
+	 {
+		 mBackgroundSound.stopMusic();
+		
+		 if(!mBackgroundSound.isCancelled())
+		 {
+			 mBackgroundSound.cancel(true);
+		 }
+		 super.onDestroy();
+	 }
+	
 }
 
 	
