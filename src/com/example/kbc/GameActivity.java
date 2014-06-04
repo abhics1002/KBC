8d57c4f8e53cdb6ed33dcc5471dc9d27ff169e85
unknown <abbharadwaj@L-MAA-00438579.corp.ebay.com>
2014-06-04 19:19:01 +0530

    GAME SCREEN commit

---
+++ src/com/example/kbc/GameActivity.java
@@ -0,0 +1,28 @@
+package com.example.kbc;
+
+import com.example.kbc.util.SystemUiHider;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ *
+ * @see SystemUiHider
+ */
+public class GameActivity extends Activity {
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_game);
+
+		
+	}
+}
+
+	
